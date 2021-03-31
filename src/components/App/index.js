9f558bc0a2c90120b6a76705ce7ff6ea55b4b427
Kyle <kylefredin@gmail.com>
2021-03-30 20:20:02 -0600

    app changes

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Shows from "../../containers/Shows";
+
+function App() {
+  return (
+    <div className="App">
+      <Shows />
+    </div>
+  );
+}
+
+export default App;
